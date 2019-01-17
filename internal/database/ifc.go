6531751da48b4b2a44cabd2d25e0bbff2d1be0f1
Amandine Drebes <amandine@energie-ip.com>
2019-01-17 18:11:40 +0100

    Allow Mac address to be empty when the driver is not setup

--- internal/database/ifc.go
+++ internal/database/ifc.go
@@ -4,8 +4,8 @@ import (
 	"github.com/energieip/srv200-coreservice-go/internal/core"
 )
 
-func GetIfcs(db Database) map[string]core.IfcInfo {
-	res := make(map[string]core.IfcInfo)
+func GetIfcs(db Database) []core.IfcInfo {
+	var res []core.IfcInfo
 	projects := GetProjects(db)
 	models := GetModels(db)
 
@@ -15,14 +15,14 @@ func GetIfcs(db Database) map[string]core.IfcInfo {
 			continue
 		}
 
-		res[project.Mac] = core.IfcInfo{
+		res = append(res, core.IfcInfo{
 			Label:      project.Label,
 			ModelName:  model.Name,
 			Mac:        project.Mac,
 			Vendor:     model.Vendor,
 			URL:        model.URL,
 			DeviceType: model.DeviceType,
-		}
+		})
 	}
 	return res
 }
