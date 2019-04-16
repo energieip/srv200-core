a22b98ec9af97e742451f639b91ed359e9dccbdc
Amandine Drebes <amandine@energie-ip.com>
2019-04-16 16:19:30 +0200

    Add the possibility to change the modbusID

--- internal/database/project.go
+++ internal/database/project.go
@@ -24,6 +24,9 @@ func SaveProject(db Database, cfg core.Project) error {
 	if cfg.ModelName != nil {
 		proj.ModelName = cfg.ModelName
 	}
+	if cfg.ModbusID != 0 {
+		proj.ModbusID = cfg.ModbusID
+	}
 	err := db.UpdateRecord(ConfigDB, ProjectsTable, dbID, proj)
 	return err
 
