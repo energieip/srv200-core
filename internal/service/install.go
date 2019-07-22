c01a480073db2ceb07f09e7f6c8c34887ee64e66
Amandine Drebes <amandine@energie-ip.com>
2019-07-22 10:56:08 +0200

    Allow set LED in firstDay based on IFC configuration file

--- internal/service/install.go
+++ internal/service/install.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	gm "github.com/energieip/common-components-go/pkg/dgroup"
 	"github.com/energieip/common-components-go/pkg/pconst"
 
 	db "github.com/energieip/common-components-go/pkg/dblind"
@@ -41,13 +42,51 @@ func (s *CoreService) installDriver(dr interface{}) {
 		elt.Mac = driver.Mac
 		database.SaveLedLabelConfig(s.db, *elt)
 
-		switchConf := sd.SwitchConfig{}
-		switchConf.Mac = elt.SwitchMac
-		switchConf.LedsSetup = make(map[string]dl.LedSetup)
-		switchConf.LedsSetup[elt.Mac] = *elt
-		url := "/write/switch/" + elt.SwitchMac + "/update/settings"
-		dump, _ := switchConf.ToJSON()
-		s.server.SendCommand(url, dump)
+		if elt.SwitchMac != "" {
+			switchConf := sd.SwitchConfig{}
+			switchConf.Mac = elt.SwitchMac
+			switchConf.LedsSetup = make(map[string]dl.LedSetup)
+			switchConf.LedsSetup[elt.Mac] = *elt
+			url := "/write/switch/" + elt.SwitchMac + "/update/settings"
+			dump, _ := switchConf.ToJSON()
+			s.server.SendCommand(url, dump)
+		}
+
+		groupCfg, _ := database.GetGroupConfig(s.db, *elt.Group)
+		newLeds := []string{}
+		for _, led := range groupCfg.Leds {
+			if led != elt.Mac {
+				newLeds = append(newLeds, led)
+			}
+		}
+		newLeds = append(newLeds, elt.Mac)
+		groupCfg.Leds = newLeds
+
+		if elt.FirstDay != nil && *elt.FirstDay == true {
+			firstDay := []string{}
+			for _, led := range groupCfg.FirstDay {
+				if led != elt.Mac {
+					firstDay = append(firstDay, led)
+				}
+			}
+			firstDay = append(firstDay, elt.Mac)
+			groupCfg.FirstDay = firstDay
+		}
+
+		database.UpdateGroupConfig(s.db, *groupCfg)
+		newSwitch := database.GetGroupSwitchs(s.db, groupCfg.Group)
+		for sw := range newSwitch {
+			if sw == "" {
+				continue
+			}
+			url := "/write/switch/" + sw + "/update/settings"
+			switchSetup := sd.SwitchConfig{}
+			switchSetup.Mac = sw
+			switchSetup.Groups = make(map[int]gm.GroupConfig)
+			switchSetup.Groups[groupCfg.Group] = *groupCfg
+			dump, _ := switchSetup.ToJSON()
+			s.server.SendCommand(url, dump)
+		}
 	case pconst.BLIND:
 		elt, _ := database.GetBlindLabelConfig(s.db, proj.Label)
 		if elt == nil {
@@ -58,13 +97,15 @@ func (s *CoreService) installDriver(dr interface{}) {
 		database.SaveBlindLabelConfig(s.db, *elt)
 
 		// send allow new driver configuration to the switch
-		switchConf := sd.SwitchConfig{}
-		switchConf.Mac = elt.SwitchMac
-		switchConf.BlindsSetup = make(map[string]db.BlindSetup)
-		switchConf.BlindsSetup[elt.Mac] = *elt
-		url := "/write/switch/" + elt.SwitchMac + "/update/settings"
-		dump, _ := switchConf.ToJSON()
-		s.server.SendCommand(url, dump)
+		if elt.SwitchMac != "" {
+			switchConf := sd.SwitchConfig{}
+			switchConf.Mac = elt.SwitchMac
+			switchConf.BlindsSetup = make(map[string]db.BlindSetup)
+			switchConf.BlindsSetup[elt.Mac] = *elt
+			url := "/write/switch/" + elt.SwitchMac + "/update/settings"
+			dump, _ := switchConf.ToJSON()
+			s.server.SendCommand(url, dump)
+		}
 	case pconst.HVAC:
 		elt, _ := database.GetHvacLabelConfig(s.db, proj.Label)
 		if elt == nil {
@@ -75,13 +116,15 @@ func (s *CoreService) installDriver(dr interface{}) {
 		database.SaveHvacLabelConfig(s.db, *elt)
 
 		// send allow new driver configuration to the switch
-		switchConf := sd.SwitchConfig{}
-		switchConf.Mac = elt.SwitchMac
-		switchConf.HvacsSetup = make(map[string]dh.HvacSetup)
-		switchConf.HvacsSetup[elt.Mac] = *elt
-		url := "/write/switch/" + elt.SwitchMac + "/update/settings"
-		dump, _ := switchConf.ToJSON()
-		s.server.SendCommand(url, dump)
+		if elt.SwitchMac != "" {
+			switchConf := sd.SwitchConfig{}
+			switchConf.Mac = elt.SwitchMac
+			switchConf.HvacsSetup = make(map[string]dh.HvacSetup)
+			switchConf.HvacsSetup[elt.Mac] = *elt
+			url := "/write/switch/" + elt.SwitchMac + "/update/settings"
+			dump, _ := switchConf.ToJSON()
+			s.server.SendCommand(url, dump)
+		}
 	case pconst.SENSOR:
 		elt, _ := database.GetSensorLabelConfig(s.db, proj.Label)
 		if elt == nil {
@@ -92,13 +135,15 @@ func (s *CoreService) installDriver(dr interface{}) {
 		database.SaveSensorLabelConfig(s.db, *elt)
 
 		// send allow new driver configuration to the switch
-		switchConf := sd.SwitchConfig{}
-		switchConf.Mac = elt.SwitchMac
-		switchConf.SensorsSetup = make(map[string]ds.SensorSetup)
-		switchConf.SensorsSetup[elt.Mac] = *elt
-		url := "/write/switch/" + elt.SwitchMac + "/update/settings"
-		dump, _ := switchConf.ToJSON()
-		s.server.SendCommand(url, dump)
+		if elt.SwitchMac != "" {
+			switchConf := sd.SwitchConfig{}
+			switchConf.Mac = elt.SwitchMac
+			switchConf.SensorsSetup = make(map[string]ds.SensorSetup)
+			switchConf.SensorsSetup[elt.Mac] = *elt
+			url := "/write/switch/" + elt.SwitchMac + "/update/settings"
+			dump, _ := switchConf.ToJSON()
+			s.server.SendCommand(url, dump)
+		}
 	case pconst.SWITCH:
 		elt := database.GetSwitchLabelConfig(s.db, proj.Label)
 		if elt == nil {
@@ -107,11 +152,6 @@ func (s *CoreService) installDriver(dr interface{}) {
 		}
 		elt.Mac = &driver.Mac
 		database.SaveSwitchLabelConfig(s.db, *elt)
-
-		switchConf := sd.SwitchConfig{}
-		switchConf.Mac = *elt.Mac
-		url := "/write/switch/" + *elt.Mac + "/update/settings"
-		dump, _ := switchConf.ToJSON()
-		s.server.SendCommand(url, dump)
+		//configuration will be sent with the next hello. It will avoid duplicate code.
 	}
 }
